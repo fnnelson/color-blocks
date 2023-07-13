dde829edd73f433254f247050c22f278a3de7421
Forrest Nelson <forrestnnelson@gmail.com>
2023-07-13 16:50:56 -0500

    updated script.js, able to add squares using functions for every color

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ $(document).ready(onReady);
 function onReady() {
     console.log('🟥 🟦 🟩 🟨 ready!!'); //making sure the function is working when page is ready
 
-    $('#red-btn').on('click', addRedSquare)
-    
+    $('#red-btn').on('click', addRedSquare);
+    $('#blue-btn').on('click', addBlueSquare);
+    $('#green-btn').on('click', addGreenSquare);
+    $('#yellow-btn').on('click', addYellowSquare);
 
 
 
@@ -15,5 +17,26 @@ function addRedSquare() {
     // console.log("in addRedSquare function");
     $('#blocks').append(`
     <div class="block red-fill"></div>
+    `)
+}
+
+function addBlueSquare() {
+    // console.log("in addBlueSquare function");
+    $('#blocks').append(`
+    <div class="block blue-fill"></div>
+    `)
+}
+
+function addGreenSquare() {
+    // console.log("in addGreenSquare function");
+    $('#blocks').append(`
+    <div class="block green-fill"></div>
+    `)
+}
+
+function addYellowSquare() {
+    // console.log("in addYellowSquare function");
+    $('#blocks').append(`
+    <div class="block yellow-fill"></div>
     `)
 }
